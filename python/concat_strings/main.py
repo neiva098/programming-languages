27cafa40040a446282b4ef2a9d1d4de8937ac1cc
Cristiano <neivacristiano@yahoo.com.br>
2021-04-19 23:27:16 -0300

    feature: type class

---
+++ python/concat_strings/main.py
@@ -0,0 +1,14 @@
+x = 10
+
+print("x é igual a %i" %x)
+print("x é igual a " + str(x))
+print("x é igual a ", x)
+
+num_dec = 7.4
+
+print("num_dec igual a", num_dec)
+print("num_dec é igual a %.10f" %num_dec)
+
+str = "ate mais"
+
+print("str é igual a %s" %str)
---
+++ python/identation/main.py
@@ -0,0 +1,3 @@
+if True: print("Ola"); 
+
+x=2; x=3; print(x);
